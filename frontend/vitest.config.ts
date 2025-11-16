29dc225d25b45fd3f1aecb1aa96786c48e26950c
Darlan Raimundo <darlanraimundo123@gmail.com>
2025-11-16 17:27:19 -0300

    feat: add inline dependencies for Vitest configuration

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -5,6 +5,9 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     setupFiles: './src/setupTests.ts',
+    deps: {
+      inline: ['whatwg-url', 'webidl-conversions'],
+    },
     coverage: {
       reporter: ['text', 'lcov'],
     },
